24f95daf61c91d909f83c1a68184fd9a946d795c
Mo-Sami <eng.muhamed.samy@gmail.com>
2019-10-25 10:42:31 +0200

    creat add and sub methods

--- src/tsetproject/Calculator.java
+++ src/tsetproject/Calculator.java
@@ -3,16 +3,19 @@ package tsetproject;
 
 public class Calculator {
     private int a ,b;
-    private double x , y;
     public Calculator(int a, int b){
         this.a = a;
         this.b = b;
     }
-    public Calculator(double a, double b){
-        this.x = a;
-        this.y = b;
-    }
+  
     public Calculator(){
         
     }
+    public static int add(int a, int b){
+        return a + b;
+    }
+     public static int sub(int a, int b){
+        return a - b;
+    }
+    
 }
