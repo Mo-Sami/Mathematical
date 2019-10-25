62bd8cca2fbc3d0fc27c7d8e9977c719ba95438b
yasmineka12345 <yassminkamal92@gmail.com>
2019-10-25 18:32:58 +0200

    mergrr

--- src/tsetproject/ArraysOperations.java
+++ src/tsetproject/ArraysOperations.java
@@ -6,19 +6,7 @@ public class ArraysOperations {
     public ArraysOperations(int []arr){
         this.array = arr;
     }
-       private void mergeSort(int []arr,int lb, int ub){
-        if(lb < ub){
-           int mid = (lb + ub)/2;
-            mergeSort(arr, lb, mid);
-            mergeSort(arr, mid+1, ub);
-            merge(arr, lb, mid,ub);
-           
       
-        }}  
-
-         private void merge(int []arr, int lb, int mid, int ub){
-             
-         }
     
     public int[] insertionSort(int[]array){
         int i =1;
