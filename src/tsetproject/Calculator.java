2148b8a195a8f2e70f75f32b8c5516e15693d890
Mo-Sami <eng.muhamed.samy@gmail.com>
2019-10-25 10:16:42 +0200

    create constructor

---
+++ src/tsetproject/Calculator.java
@@ -0,0 +1,18 @@
+
+package tsetproject;
+
+public class Calculator {
+    private int a ,b;
+    private double x , y;
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public Calculator(double a, double b){
+        this.x = a;
+        this.y = b;
+    }
+    public Calculator(){
+        
+    }
+}
