8bced7867c6092e136e53fe3e936d5e0a62c646c
yasmineka12345 <yassminkamal92@gmail.com>
2019-10-25 16:44:50 +0200

    insertion

--- src/tsetproject/ArraysOperations.java
+++ src/tsetproject/ArraysOperations.java
@@ -6,4 +6,24 @@ public class ArraysOperations {
     public ArraysOperations(int []arr){
         this.array = arr;
     }
+    
+    
+    public int[] insertionSort(int[]array){
+        int i =1;
+        int key;
+        int j;
+        while(i<array.length){
+            key=array[i];
+            j=i-1;
+        while(j>=0&&array[j]<key){
+            array[j+1]=array[j];
+            j=j-1;
+        }
+        array[j+1]=key;
+        i=i+1;
+        }
+        return array;
+    }
+    
+    
 }
