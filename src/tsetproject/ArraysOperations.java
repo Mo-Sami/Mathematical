eb036c90b10bac2df350f462fe1ff21ec4f404ef
yasmineka12345 <yassminkamal92@gmail.com>
2019-10-25 18:48:17 +0200

    add merging sort

--- src/tsetproject/ArraysOperations.java
+++ src/tsetproject/ArraysOperations.java
@@ -5,8 +5,43 @@ public class ArraysOperations {
     private int[] array ;
     public ArraysOperations(int []arr){
         this.array = arr;
+    
     }
-      
+        private void mergeSort(int []arr,int lb, int ub){
+        if(lb < ub){
+           int mid = (lb + ub)/2;
+            mergeSort(arr, lb, mid);
+            mergeSort(arr, mid+1, ub);
+            merge(arr, lb, mid,ub);
+           
+        }
+            
+            
+        }
+     private void merge(int []arr, int lb, int mid, int ub){
+         int i ,j,k;
+         i=j=k=0;
+         int []result = new int [arr.length];
+         while(i <= mid && j <= ub){ 
+             if(arr[i] <= arr [j])
+                 result[k++] = arr[i++];
+             else
+                 result[k++] = arr[j++];
+             
+         }
+         if(i> mid){
+             while(j <= ub){
+             result[k++] = arr[j];}}
+    else{
+             while(i <= mid ){
+                 result[k++] = arr[i];
+             }
+         }
+         for (k= lb ; k < ub; k++) {
+             arr[k] = result[k];
+             
+         }
+     }
     
     public int[] insertionSort(int[]array){
         int i =1;
